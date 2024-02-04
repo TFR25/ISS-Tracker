ec7e27dedc8dbbf855b4386b0c0fb4d811ce94ba
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-02-04 11:07:57 -0800

    Get ISS latitude and longitude position

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import requests
 
 api_data = requests.get(url="http://api.open-notify.org/iss-now.json")
 api_data.raise_for_status()
+data = api_data.json()
 
 
-
+def get_iss_data():
+    """get ISS latitude and longitude position"""
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    return iss_latitude, iss_longitude
