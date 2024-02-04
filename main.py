5998778214e741741231d85a6bcb40c054bfa2e1
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-02-04 11:10:42 -0800

    Get current time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 api_data = requests.get(url="http://api.open-notify.org/iss-now.json")
 api_data.raise_for_status()
@@ -10,3 +11,11 @@ def get_iss_data():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
     return iss_latitude, iss_longitude
+
+
+def get_path_time():
+    """get current time"""
+    now = datetime.now()
+    iss_path_date = now.strftime("%m-%d-%y")
+    iss_path_time = now.strftime("%H:%M:%S")
+    return iss_path_date, iss_path_time
