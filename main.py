2eef418efd666c5f8cef57e0c314f3a64671b997
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-02-05 10:36:19 -0800

    Update ISS symbol, map layout and add legend.

--- main.py
+++ main.py
@@ -14,14 +14,11 @@ data = api_data.json()
 def view_point_location():
     with open("data.json") as file:
         view_point_position = json.load(file)
-        # replace view_point_lat and view_point_lng with your latitude and longitude. Utilize this website to find your
-        # latitude and longitude https://www.latlong.net
+        # replace view_point_lat and view_point_lng with your latitude and longitude. You can utilize this website
+        # https://www.latlong.net to find your latitude and longitude.
         view_point_lat = view_point_position["my_location"]["lat"]
         view_point_lng = view_point_position["my_location"]["lng"]
         return view_point_lat, view_point_lng
-
-
-view_lat, view_lng = view_point_location()
 
 
 def get_iss_data():
@@ -39,6 +36,9 @@ def get_path_time():
     return iss_path_date, iss_path_time
 
 
+view_lat, view_lng = view_point_location()
+
+
 def path_time_history():
     """save ISS position date and time history to csv file for mapping"""
     with open("iss_data_path_history.csv", "a", newline="") as file:
@@ -47,20 +47,34 @@ def path_time_history():
         date, time = get_path_time()
         iss_data = [latitude, longitude, date, time, view_lat, view_lng]
         writer.writerow(iss_data)
-
-
-path_time_history()
+        return
 
 
 def plotter():
+    path_time_history()
     df = pandas.read_csv("iss_data_path_history.csv")
-    figure = px.scatter_geo(df, lat="latitude", lon="longitude")
-    figure.update_layout(title="World Map", title_x=0.5)
-    figure.add_scattergeo(lat=df["view_point_lat"], lon=df["view_point_lng"],
-                          mode='markers', marker=dict(size=18, symbol="star", color="red"))
+    figure = px.scatter_geo()
+    figure.update_layout(title="ISS Tracker", title_x=0.5, legend=dict(
+        title="Legend:",
+        yanchor="top",
+        y=-5,
+        xanchor="left",
+        x=0.4,
+        orientation="h"
+    ))
+    figure.update_layout(height=600, margin={"r": 0, "t": 50, "l": 0, "b": 0}, autosize=True)
+
+    figure.add_scattergeo(lat=df["view_lat"], lon=df["view_lng"],
+                          mode="markers", name="Washington State  ", marker=dict(size=16, symbol="star", color="red"))
+
+    figure.add_scattergeo(lat=df["latitude"], lon=df["longitude"],
+                          mode="markers", name="ISS", marker={"size": 16, "symbol": "arrow", "angleref": "previous",
+                                                              "color": "Blue",
+                                                              "line": dict(width=2, color="DarkTurquoise")})
+
     figure.show()
 
 
 plotter()
 
-distance = haversine(get_iss_data(), view_point_location(), unit='mi')
+distance = haversine(get_iss_data(), view_point_location(), unit="mi")
